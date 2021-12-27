634eeafab878e29e31f73e2cb9f28f5445c18783
hekang42 <hekang@student.42seoul.kr>
2021-12-27 22:30:52 +0900

    wp setting

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -9,4 +9,9 @@ rm -r /wordpress latest.tar.gz
 cp tmp/wp-config.php				/var/www/html/wp-config.php
 cp tmp/www.conf		/etc/php/7.3/fpm/pool.d/www.conf
 
+wp core install --allow-root --url=$DOMAIN_NAME --title='WordPress' --admin_user=$WP_USER1\
+	--admin_password=$WP_USER1_PW --admin_email="hekang@student.42seoul.kr" --path='/var/www/html'
+
+wp user create --allow-root $WP_USER2 sushin@student.42seoul.kr --user_pass=$WP_USER2_PW --role=author --path='/var/www/html'
+
 php-fpm7.3 --nodaemonize
